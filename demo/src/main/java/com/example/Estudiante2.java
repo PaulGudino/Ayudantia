f0406de04a8487ebbeb6265b0ffb5e7ecbeb364f
PaulGudino <pgudino@espol.edu.ec>
2024-05-30 11:24:15 -0500

    Estudiante 2 new data

--- demo/src/main/java/com/example/Estudiante2.java
+++ demo/src/main/java/com/example/Estudiante2.java
@@ -1,1 +1,12 @@
+package com.example;
+
+public class Estudiante2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Estudiante 1 modifico!");
+        System.out.println("Hello Estudiante 1 modifico!");
+        System.out.println("Hello Estudiante 1 modifico!");
+        System.out.println("Hello Estudiante 1 modifico!");
+        System.out.println("Hello Estudiante 1 modifico!");
+    }
+}
 
