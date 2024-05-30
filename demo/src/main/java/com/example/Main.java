ac387c9045679345a2dd2a2c84a24eefb5d8da3e
PaulGudino <pgudino@espol.edu.ec>
2024-05-30 11:17:37 -0500

    Estudiante 2 update

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,13 +2,13 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
-        System.out.println("Hello Principal!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
+        System.out.println("Hello ESPOL!");
     }
 }
